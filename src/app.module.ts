20d01e9c512f4128ef6a0af09e828a6972896f1f
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-22 17:20:15 -0500

    Refactor AppService to inject API_KEY and update getHello method to include API Key in response

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = 'sdadsdsaddsa12312asdsa';
+const API_KEY_PROD = 'PROD123';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
