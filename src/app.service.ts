60cacac4ca749b14a70182c1afc146a9f0bf6828
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-22 19:35:39 -0500

    Add HttpModule and TASKS provider to AppModule; inject TASKS into AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,13 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private readonly apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private readonly apiKey: string,
+    @Inject('TASKS') private readonly tasks: string[],
+  ) {}
 
   getHello(): string {
+    console.log('Tasks:', this.tasks);
     return `Hello World! My API Key is ${this.apiKey}`;
   }
 }
