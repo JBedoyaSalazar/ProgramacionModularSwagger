0f5d7017c5a9f1a844f9635ded1ff608832c61e1
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-27 16:25:06 -0500

    feat: add Mongoose integration and update database module for MongoDB connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,22 @@
 import { Module, Global } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { env } from '../config/env.interface';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<env>) => ({
+        uri: configService.get<string>('DB_URI'),
+        dbName: configService.get<string>('DB_NAME'),
+      }),
+    }),
+  ],
   providers: [DatabaseService],
-  exports: [DatabaseService],
+  exports: [DatabaseService, MongooseModule],
 })
 export class DatabaseModule {}
