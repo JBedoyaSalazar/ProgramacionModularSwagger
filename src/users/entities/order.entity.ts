b63646b2176996a1206398f8139b24f92c0faa5b
Jefred Bedoya <jefred03salazar@gmail.com>
2026-08-05 01:48:03 -0500

    feat: add ClassSerializerInterceptor and enhance Order, OrderProduct, and User entities with Expose and Exclude decorators

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -7,6 +7,7 @@ import {
   UpdateDateColumn,
   OneToMany,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 
 import { Customer } from './customer.entity';
 import { OrderProduct } from './orderProduct.entity';
@@ -22,9 +23,11 @@ export class Order {
   @JoinColumn({ name: 'customer_id' })
   customer!: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderProduct, (orderProduct) => orderProduct.order)
   items!: OrderProduct[];
 
+  @Exclude()
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamptz',
@@ -32,10 +35,38 @@ export class Order {
   })
   createdAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   updatedAt: Date;
+
+  @Expose()
+  get products() {
+    if (this.items && this.items.length > 0) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          itemId: item.id,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items && this.items.length > 0) {
+      return this.items.reduce((acc, item) => {
+        if (item && item.product) {
+          return acc + item.product.price * item.quantity;
+        }
+        return acc;
+      }, 0);
+    }
+    return 0;
+  }
 }
--- src/users/entities/orderProduct.entity.ts
+++ src/users/entities/orderProduct.entity.ts
@@ -7,6 +7,7 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 import { Product } from '../../products/entities/product.entity';
 import { Order } from './order.entity';
@@ -16,6 +17,7 @@ export class OrderProduct {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Exclude()
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamptz',
@@ -23,6 +25,7 @@ export class OrderProduct {
   })
   createdAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
