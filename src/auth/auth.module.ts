93768c435e282d1fb2663adf7b77a507ef9f3b5e
Jefred Bedoya <jefred03salazar@gmail.com>
2026-08-08 17:00:55 -0500

    feat(auth): add AuthController with login endpoint and local authentication guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { UsersModule } from '../users/users.module';
 
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
