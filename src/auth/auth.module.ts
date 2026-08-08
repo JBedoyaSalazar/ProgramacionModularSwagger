aaedf44c984b127036fc305b49ce217dbed86a77
Jefred Bedoya <jefred03salazar@gmail.com>
2026-08-08 15:18:48 -0500

    feat(auth): implement local authentication strategy with bcrypt password validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+import { UsersModule } from '../users/users.module';
+
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
+})
 export class AuthModule {}
---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from '../../users/services/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    const isPasswordValid = await this.comparePasswords(
+      password,
+      user.password,
+    );
+
+    if (user && isPasswordValid) {
+      return user;
+    }
+
+    return null;
+  }
+
+  private async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+}
